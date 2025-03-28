965ddbbdb3aafb8c7ed8ffd5f11ba04abe24739b
hectormarin24 <129911072+hectormarin24@users.noreply.github.com>
2025-03-27 21:02:59 -0700

    Create echo.java

---
+++ echo.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class echo{
+
+
+    class membership{
+
+        String name;
+        String email;
+        int number;
+        String address;
+        String password;
+        String username;
+        String dob;
+
+    }
+
+
+    public static void main(String[] args){
+
+        
+
+    }
+}
