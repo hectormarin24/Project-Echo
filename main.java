d1660164bc4b2b4e044d4ca09dd2e06260ccf2c8
hectormarin24 <129911072+hectormarin24@users.noreply.github.com>
2025-03-28 13:56:31 -0700

    change of name from echo.java to main.java

---
+++ main.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class main{
+
+
+    class membership{
+
+        String Fname;
+        String Lname;
+        String Minitial;
+        String email;
+        int number;
+        String address;
+        String password;
+        String username;
+        String dob;
+
+    }
+
+
+    public static void main(String[] args){
+
+        
+
+    }
+}
