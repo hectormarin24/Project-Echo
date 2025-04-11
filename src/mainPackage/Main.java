a349a098db7256d11283a608f8e8670de4fb57b6
hectormarin <hectormarin@UL-ARC1003-1014.saclink.csus.edu>
2025-04-11 08:52:07 -0700

    got sql and javafx to work together at the same time under main. Added a src folder

--- src/mainPackage/Main.java
+++ src/mainPackage/Main.java
@@ -1,11 +1,44 @@
 package mainPackage;
 //import java.util.*;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Main{
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) throws ClassNotFoundException, SQLException{
+		launch(args);
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+	       
+       Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "Echo215555$");
+       System.out.println("Connection created");
+	}
+}
+
+
+
+
+/*public class Main{
 
 
     
@@ -24,4 +57,4 @@ public class Main{
        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "Echo215555$");
        System.out.println("Connection created");
     }
-}
+}*/
