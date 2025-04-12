6624a504e03b122e86a9c456f1b5eb703f7c9b87
hectormarin <hectormarin@UL-ARC1003-1014.saclink.csus.edu>
2025-04-12 12:34:14 -0700

    updated main.java to comment out the mySQL portion since we wont be using it, but I want to have it there just in case we do in the future

--- src/mainPackage/Main.java
+++ src/mainPackage/Main.java
@@ -6,9 +6,9 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-import java.sql.Connection;
+/*import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.SQLException;*/
 
 
 public class Main extends Application {
@@ -25,36 +25,24 @@ public class Main extends Application {
 		}
 	}
 	
-	public static void main(String[] args) throws ClassNotFoundException, SQLException{
+	public static void main(String[] args) throws ClassNotFoundException{
 		launch(args);
 		
-		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	   /*Class.forName("com.mysql.cj.jdbc.Driver");
 	       
        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "Echo215555$");
-       System.out.println("Connection created");
+       System.out.println("Connection created");*/
 	}
 }
 
-
-
-
-/*public class Main{
-
-
-    
-
-    class Administrator{
-        String role;
-        String name;
-        String email;
-        int adminid;
-    }
-
-    public static void main(String[] args) throws ClassNotFoundException, SQLException{
-
-       Class.forName("com.mysql.cj.jdbc.Driver");
-       
-       Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "Echo215555$");
-       System.out.println("Connection created");
-    }
-}*/
