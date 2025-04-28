4124a31390987fa9df2ba29c4321511c4e592524
aa-alan <126564988+aa-alan@users.noreply.github.com>
2025-04-28 03:01:59 -0700

    Some more bug fixes. DBWishlist method additions

--- src/mainPackage/DBWishlist.java
+++ src/mainPackage/DBWishlist.java
@@ -9,35 +9,57 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 public class DBWishlist {
+	private static final String DB_URL = "jdbc:sqlite:db/echo.db";
+    
+    
+    private static Connection connect() {
+        try {
+            return DriverManager.getConnection(DB_URL);
+        } catch (SQLException e) {
+            System.out.println("Connection failed: " + e.getMessage());
+            return null;
+        }
+    }
 	
-	public DBWishlist() {
-		String url = "jdbc:sqlite:db/echo.db";
-		
+	public static void createTable() {
 		String sql = "CREATE TABLE IF NOT EXISTS wishlists ("
 	            + "	userID INTEGER,"
 	            + "	bookID INTEGER,"
 	            + " PRIMARY KEY (userID, bookID)"
 	            + ");";
 		
-		try (Connection conn = DriverManager.getConnection(url);
+		try (Connection conn = connect();
 	         Statement stmt = conn.createStatement()) {
 	            stmt.execute(sql);
+	            System.out.println("Table created or already exists.");
 	    } catch (SQLException e) {
 	            System.out.println(e.getMessage());
 	    }
 	}
 	
+	public static void deleteTable() {
+    	String sql = "DROP TABLE IF EXISTS wishlists";
+    	
+    	try(Connection conn = connect();
+    		Statement stmt = conn.createStatement()) {
+    		
+    		stmt.execute(sql);
+    		System.out.println("Table 'wishlists' has been dropped (if it existed).");
+    		
+    	} catch (SQLException e) {
+    		e.printStackTrace();
+    	}
+    }
+	
 	// Find all books that are connected to the user account within the database 
 	// and return the data via an ArrayList. This data will be loaded by the front end.
-	public ArrayList<Integer> getWishlistedBookIDs(int userID) {
+	public static ArrayList<Integer> getWishlistedBookIDs(int userID) {
 		ArrayList<Integer> bookIDs = new ArrayList<>();
-		String url = "jdbc:sqlite:db/echo.db";
-		
 		String sql = "SELECT bookID"
 				+ " FROM wishlists"
                 + " WHERE userID = ?;";
 		
-		try (Connection conn = DriverManager.getConnection(url);
+		try (Connection conn = connect();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
 				pstmt.setInt(1, userID);
 				
@@ -56,15 +78,13 @@ public class DBWishlist {
 	// Call this together with "checkAvailability" when loading the wish list window/tab.
 	// Return value: true = the user CAN wish list the book since they haven't wish listed it yet.
 	// Return value: false = the user CANNOT wish list the book since they have wish listed it already.
-	public boolean canWishlist(int userID, int bookID) {
-		String url = "jdbc:sqlite:db/echo.db";
-		
+	public static boolean canWishlist(int userID, int bookID) {
 		String sql = "SELECT userID, bookID"
 				+ " FROM wishlists"
 				+ " WHERE userID = ?"
 				+ " AND bookID = ?;";
 		
-		try (Connection conn = DriverManager.getConnection(url);
+		try (Connection conn = connect();
 	         PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
 	            pstmt.setInt(1, userID);
@@ -86,15 +106,13 @@ public class DBWishlist {
 	// Front end calls this function providing the userID and bookID.
 	// Note: front end should only show the "Wish List" button when physical availability is zero!
 	// Use the "checkAvailability" function from the BookDatabase class.
-	public void addWishlist(int userID, int bookID) {
-		String url = "jdbc:sqlite:db/echo.db";
-		
+	public static void addWishlist(int userID, int bookID) {
 		String sql = "INSERT INTO wishlists"
 				+ " (userID, bookID)"
 				+ " VALUES"
 				+ " (?, ?);";
 		
-		try (Connection conn = DriverManager.getConnection(url);
+		try (Connection conn = connect();
 	         PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
 	            pstmt.setInt(1, userID);
@@ -109,14 +127,12 @@ public class DBWishlist {
 	
 	// A user has just clicked on the "Remove" button on the wish list page on the book they want to remove.
 	// The front end should call this function, providing the user's ID and the book's ID.
-	public void removeWishlist(int userID, int bookID) {
-		String url = "jdbc:sqlite:db/echo.db";
-		
+	public static void removeWishlist(int userID, int bookID) {	
 		String sql = "DELETE FROM wishlists"
 				+ " WHERE userID = ?"
 				+ " AND bookID = ?;";
 		
-		try (Connection conn = DriverManager.getConnection(url);
+		try (Connection conn = connect();
 	         PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
 	            pstmt.setInt(1, userID);
@@ -130,12 +146,10 @@ public class DBWishlist {
 	}
 	
 	// Call at the start of the program.
-	public void pingWishlists() {
-		String url = "jdbc:sqlite:db/echo.db";
-		
+	public static void pingWishlists() {
 		String sql = "SELECT * FROM wishlists;";
 		
-		try (Connection conn = DriverManager.getConnection(url);
+		try (Connection conn = connect();
 	         Statement stmt = conn.createStatement();
 			 ResultSet rs = stmt.executeQuery(sql)) {
 
